5e3510c439f3fb43ffcf9a00fee7aa813e9a25c7
Natalie Cygan <nataliemcygan@gmail.com>
2018-11-28 23:30:14 -0800

    Added buttons to events page, added button images to assets

--- spark/App.js
+++ spark/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import {Platform, StyleSheet, Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { createStore } from "redux";
 
